9d4c8f3452b2a9278d147e5e0fe739f7790716a6
Phillip Kelly <phillipbentonkelly@gmail.com>
2020-04-16 18:00:37 -0400

    Use travis env variables in tests.

--- src/tools/encryptJson.js
+++ src/tools/encryptJson.js
@@ -1,9 +1,9 @@
 const cryptoJSON = require('crypto-json');
 const config = require('dotenv').config({ path: './.env' }).parsed;
-const password = config.password;
+const password = config.password || "$CRYPTO_PASSWORD";
 const keys = [];
-const algorithm = config.algorithm;
-const encoding = config.encoding;
+const algorithm = config.algorithm || "$CRYPTO_ALGO";
+const encoding = config.encoding || "$CRYPTO_ENCODING";
 
 const encrypt_json = source => {
   return cryptoJSON.encrypt(
