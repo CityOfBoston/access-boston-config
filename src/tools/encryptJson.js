55a9f1f39f908659f6a6be591fe07f3bcf5ce5fd
Phillip Kelly <phillipbentonkelly@gmail.com>
2020-04-16 18:04:14 -0400

    Travis test update 1

--- src/tools/encryptJson.js
+++ src/tools/encryptJson.js
@@ -1,9 +1,9 @@
 const cryptoJSON = require('crypto-json');
 const config = require('dotenv').config({ path: './.env' }).parsed;
-const password = config.password || "$CRYPTO_PASSWORD";
+const password = config && config.password ? config.password : "$CRYPTO_PASSWORD";
 const keys = [];
-const algorithm = config.algorithm || "$CRYPTO_ALGO";
-const encoding = config.encoding || "$CRYPTO_ENCODING";
+const algorithm = config && config.password ? config.algorithm : "$CRYPTO_ALGO";
+const encoding = config && config.password ? config.encoding : "$CRYPTO_ENCODING";
 
 const encrypt_json = source => {
   return cryptoJSON.encrypt(
